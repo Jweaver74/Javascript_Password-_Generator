27cb56242c44179b9a7cf8872a68425d52b892bf
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-03-10 10:14:55 -0600

    working on password, not displaying properly

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ var generateBtn = document.querySelector("#generate");
   var numbers = ["0","1","2","3","4","5","6","7","8","9"];
   var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var special = ["!","@","#","$","%","^","&","*","'","(",")","+",",","-",".","/",":",";","<",">","=","?","[","]","{","}",""];
+  var special = ["!","@","#","$","%","^","&","*","'","(",")","+",",","-",".","/",":",";","<",">","=","?","[","]","{","}"];
 
 
 function questions(){
   var isValid = false;
   do{
     var length = prompt("Choose password length between 8 and 128");
-    var askNumbers = confirm("Do you to inculde numbers in you password?");
-    var askLowercase = confirm("Do you want to include lower case letters in your password?");
-    var askUppercase = confirm("Do you want to include upper case letters in your password?");
-    var askSpecial = confirm("Do you want to include special characters in your password?");
+    var askNumbers = confirm("Do you to include numbers in you password? Press OK for yes or cancel for no.");
+    var askLowercase = confirm("Do you want to include lower case letters in your password? Press Ok for yes or cancel for no.");
+    var askUppercase = confirm("Do you want to include upper case letters in your password? Press Ok for yes or cancel for no.");
+    var askSpecial = confirm("Do you want to include special characters in your password? Press Ok for yes or cancel for no.");
     var responses = {
       length: length,
       askNumbers: askNumbers,
@@ -40,21 +40,18 @@ var passwordOptions = questions();
 var possibleCombo = [];
 var finalPassword = "";
 
-if (passwordOptions.askNumbers){
-  for (var i of numbers)
-  possibleCombo.push(i);
+if(passwordOptions.askNumbers){
+  possibleCombo = possibleCombo.concat(numbers);
 }
-if (passwordOptions.askLowercase){
-  for (var i of lowercase);
-  possibleCombo.push(i);
+if(passwordOptions.askLowercase){
+  possibleCombo = possibleCombo.concat(lowercase);
 }
-if (passwordOptions.askUppercase){
-  for (var i of uppercase);
-  possibleCombo.push(i);
 }
-if (passwordOptions.askSpecial){
-  for(var i of special);
-  possibleCombo.push(i);
+if(passwordOptions.askUppercase){
+  possibleCombo = possibleCombo.concat(uppercase);
+}
+if(passwordOptions.askSpecial){
+  possibleCombo = possibleCombo.concat(special);
 }
 
 console.log(possibleCombo);
